5c6993ba03215f31ebe3901f11f4548b76b1954c
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2026-03-24 14:59:04 -0700

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,8 @@
-// docs/.vitepress/theme/index.ts
-import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
-import './custom.css'
 import CustomNavButton from './CustomNavButton.vue'
+import './custom.css'
+import { h } from 'vue'
+import CodeBlock from ./components/CodeBlock.vue'
 
 export default {
   extends: DefaultTheme,
@@ -11,5 +11,8 @@ export default {
       // You can add your custom button to the 'nav-bar-content-after' or 'nav-bar-content-before' slot
       'nav-bar-content-after': () => h(CustomNavButton)
     })
+  }
+  enhanceApp({ app }) {
+    app.component('CodeBlock', CodeBlock)
   }
 }
